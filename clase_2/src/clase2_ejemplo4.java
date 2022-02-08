f4328133f7af554890185c163b15e39b1ab8f2f6
Melisa Burlando <melisa.burlando@gmail.com>
2022-02-08 13:06:38 -0300

    agrego dos archivos nuevos y edito el nombre de dos viejos

---
+++ clase_2/src/clase2_ejemplo4.java
@@ -0,0 +1,16 @@
+
+public class clase2_ejemplo4 {
+
+	public static void main(String[] args) {
+		char caracter1;
+		caracter1 = 'c';
+		char caracter2;
+		//una variable caracter2 toma el valor del caracter digito '1'
+		caracter2 = '1';
+		//una variable numero toma el valor del numero entero 1
+		int numero = 1;
+		System.out.println("El valor de caracter2 es: " + caracter2);
+		System.out.println("El valor de numero es: " + numero);
+	}
+
+}
