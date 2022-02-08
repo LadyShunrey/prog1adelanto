8474cc287fb4aea69cc3541500f0dcf334315e2d
Melisa Burlando <melisa.burlando@gmail.com>
2022-02-08 11:24:35 -0300

    agrego nuevo proyecto al repo

---
+++ clase_1/src/ejercicio_1.java
@@ -0,0 +1,7 @@
+
+public class ejercicio_1 {
+	public static void main (String[] args){
+		System.out.println("Hola Mundo");
+		System.out.println("Estoy programando");
+	}
+}
---
+++ clase_1/src/ejercicio_2.java
@@ -0,0 +1,15 @@
+
+
+public class ejercicio_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo"); 
+		
+		int cantidadCaramelos;
+		double precioCaramelos;
+		boolean quieroCaramelos;
+		char inicialMarcaCaramelos;
+		String marcaCaramelos;
+	}
+
+}
