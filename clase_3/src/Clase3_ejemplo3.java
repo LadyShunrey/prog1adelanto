f7f547830e4e2c692375c1bb1c26abd91a6d6315
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-10 17:42:11 -0300

    agrego el ejemplo 3 de la clase 3

---
+++ clase_3/src/Clase3_ejemplo3.java
@@ -0,0 +1,18 @@
+public class Clase3_ejemplo3 {
+	public static void main(String[] args) {
+		//operadores incrementales
+		int a, b;
+		a = 5;
+		a++;
+		System.out.println("a = " + a);
+		a = 5;
+		++a;
+		System.out.println("a = " + a);
+		a = 5;
+		b = a++;
+		System.out.println("a = " + a + ", b = " + b);
+		a = 5;
+		b = ++a;
+		System.out.println("a = " + a + ", b = " + b);
+	}
+}
