b687ffee3f9b7c18e749dfdcc932fc5bdc3a7f81
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-10 17:57:16 -0300

    agrego el ejemplo 4 de la clase 3

---
+++ clase_3/src/Clase3_ejemplo4.java
@@ -0,0 +1,19 @@
+
+public class Clase3_ejemplo4 {
+
+	public static void main(String[] args) {
+		int i, j;
+		double a, b;
+		i = 7;
+		j = 3;
+		a = 7.0;
+		b = 3.0;
+		i += j;
+		a /= b;
+		System.out.println("Operador combinado suma: i+=j " + i);
+		System.out.println("Operador combinado division: a/=b " + a);
+		
+		
+	}
+
+}
