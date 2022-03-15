d61787ac798c640892d3f7df7b85a39bd139d947
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-15 16:41:16 -0300

    edito ejemplo 7 de la clase 4

--- clase_4/src/Clase4_Ejemplo7.java
+++ clase_4/src/Clase4_Ejemplo7.java
@@ -2,8 +2,41 @@
 public class Clase4_Ejemplo7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*Repetición en la sentencia if...else... al final
+		*/
+		
+		int valor = 5;
+		
+		if (valor % 2 == 0) {
+			System.out.println("El valor par es:");
+			System.out.println(valor);
+		}
+		
+		else {
+			System.out.println("El valor impares:");
+			System.out.println(valor);
+		}
+
+		
+		/*Reducción en la sentencia if...else... al final
+		*/
+		
+		
+
+		if (valor % 2 == 0) {
+			System.out.println("El valor par es:");
+		}
+
+		else {
+			System.out.println("El valor impar es:");
+		}
+
+		System.out.println(valor);
+		
+		
+		
+	
 	}
 
 }
