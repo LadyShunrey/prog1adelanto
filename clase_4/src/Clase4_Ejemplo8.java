6b8ac34090703deb5444a76af8cf0204f10ba52d
Melisa Burlando <melisa.burlando@gmail.com>
2022-03-15 16:45:50 -0300

    agrego el ejemplo 8 de la clase 4

---
+++ clase_4/src/Clase4_Ejemplo8.java
@@ -0,0 +1,35 @@
+
+public class Clase4_Ejemplo8 {
+	public static void main(String[] args) {
+		
+		/*Dado un valor, verificar e imprimir si es par o impar. En el caso de que sea
+		*par verificar e imprimir si es mayor de 4 o no lo es
+		*/
+		
+		int valor = 5;
+		
+		if (valor % 2 == 0) {
+
+			System.out.println(valor + " es par");
+			
+			if (valor > 4) {
+
+				System.out.println(valor + " es mayor que 4");
+
+			}
+			
+			else {
+
+				System.out.println(valor + " es menor o igual que 4");
+
+			}
+
+		}
+		
+		else{
+
+			System.out.println(valor + " es impar");
+	
+		}
+	}
+}
