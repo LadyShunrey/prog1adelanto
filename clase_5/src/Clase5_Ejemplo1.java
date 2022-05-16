4df5ad3c298f6493e64bebd560795cbc0b88d5d2
Melisa Burlando <melisa.burlando@gmail.com>
2022-05-16 13:21:19 -0300

    agrego el ejemplo 1 de la clase 5

---
+++ clase_5/src/Clase5_Ejemplo1.java
@@ -0,0 +1,19 @@
+
+public class Clase5_Ejemplo1 {
+
+	public static void main(String[] args) {
+		
+		final int MAX = 4;
+		int numero = 1;
+		
+		while (numero <= MAX){
+			System.out.println("El valor es: " + numero);
+			numero++;
+			System.out.println("Nuevo valor es: " + numero);
+			
+			//al cambiar de valor el numero significa que el valor de la expresion va a cambiar
+		}
+
+	}
+
+}
