dd86a12875cca472478827ef7fe1f5179fff334f
Melisa Burlando <melisa.burlando@gmail.com>
2022-06-27 01:48:37 -0300

    empiezo el ejercicio 14 del practico 8

--- practico_8/src/Practico8_ejercicio14.java
+++ practico_8/src/Practico8_ejercicio14.java
@@ -9,6 +9,57 @@ patrón dada por un arreglo de caracteres de tamaño igual al tamaño de
 columnas de la matriz (solo tiene esa secuencia con separadores al inicio y al
 final). Al eliminar en cada fila se pierden los valores haciendo los corrimientos.*/
 
+/*No entiendo bien la consigna*/
+
+import java.util.Random;
 public class Practico8_ejercicio14{
+	public static final int MAXFILA = 4;
+	public static final int MAXCOLUMNA = 20;
+	public static final double PROBABILIDAD_LETRA = 0.4;
+	public static void main (String []args){
+		char [] arreglo = new char [MAXCOLUMNA];
+		char [][]matriz = new char [MAXFILA][MAXCOLUMNA];
+		cargar_matriz_secuencias_char(matriz);
+		System.out.println("La matriz: ");
+		imprimir_la_matriz(matriz);
+		cargar_arreglo_aleatorio_secuencias_char(arreglo);
+		System.out.println("El arreglo: ");
+		imprimir_el_arreglo(arreglo);
+	}
 	
+	public static void cargar_matriz_secuencias_char(char[][]matriz){
+		for (int fila = 0; fila < MAXFILA; fila++){
+			cargar_arreglo_aleatorio_secuencias_char(matriz[fila]);
+		}
+		System.out.println("");
+	}
+	
+	public static void cargar_arreglo_aleatorio_secuencias_char(char [] arr){
+		Random r = new Random();
+		arr[0] = ' ';
+		arr[MAXCOLUMNA-1] = ' ';
+		for (int pos = 1; pos < MAXCOLUMNA-1; pos++){
+			if (r.nextDouble()>PROBABILIDAD_LETRA){
+				arr[pos]=(char)(r.nextInt(26) + 'a');
+			}
+			else{
+				arr[pos]=' ';				
+			}
+		}
+	}
+	
+	public static void imprimir_la_matriz(char[][]matriz){
+		for (int fila =0; fila<MAXFILA; fila++){
+			for(int columna = 0; columna<MAXCOLUMNA; columna++){
+				System.out.print("[" + matriz[fila][columna] + "]");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void imprimir_el_arreglo(char[]arreglo){
+		for(int columna = 0; columna<MAXCOLUMNA; columna++){
+			System.out.print("[" + arreglo[columna] + "]");
+		}
+	}
 }
