48d7ccfa8808ef4342f89fda563e8b2f3497e91c
Melisa Burlando <melisa.burlando@gmail.com>
2022-06-27 02:39:10 -0300

    terminado el pseudocódigo del ejercicio 15 del practico 8

---
+++ practico_8/src/Practico8_ejercicio15.java
@@ -0,0 +1,38 @@
+/*Se tiene una matriz de caracteres de tamaño 4*20 de secuencias de
+caracteres letras minúsculas entre ‘a’ y ‘z’ (por cada fila), separadas por ‘ ’
+(espacios). La matriz esta precargada, y además cada fila empieza y termina
+con uno o más separadores ‘ ’.
+
+15. Hacer un programa que dada la matriz de secuencias de caracteres definida y
+precargada elimine todas las secuencias que tienen orden descendente entre
+sus elementos.*/
+
+public class Practico8_ejercicio15 {
+
+	public static void main(String[] args) {
+		//eliminar_secuencias_descendentes(matriz);
+	}
+
+	//public static void eliminar_secuencias_descendentes(char [][] matriz){
+		//int inicioSecuencia=0;
+		//int finSecuencia=0;
+		//int sizeSuencia =0;
+		//encontrar el inicio de una secuencia si estoy en un numero
+			//inicioSecuencia = columna;
+			//mientras el valor que sigue(en columna+1) sea menor al actual
+				//si sigue un cero(en columna+2)
+					//finSecuencia = columna+1
+					//sizeSecuencia = finSecuencia - inicioSecuencia +1;
+					//correr a izquierda matriz en fila entre inicioSecuencia y finSecuencia, sizeSecuencia veces
+	//}
+	
+	//public static void correr_a_izquierda(char[][]matriz, int fila, int inicioSecuencia, int finSecuencia, int sizeSecuencia){
+		//int cantidadDeCorridas =0;
+		//while (cantidadDeCorridas<sizeSecuencia){
+			//for(int columna = inicioSecuencia; columna<=finSecuencia; columna++){
+				//matriz[fila][columna] = matriz[fila][columna+1]; 
+			//}
+			//cantidadDeCorridas++;
+		//}
+	//}
+}
