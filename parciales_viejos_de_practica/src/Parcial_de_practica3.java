81117fe2ea0812c81b467939a3319749d541f357
Melisa Burlando <90287257+LadyShunrey@users.noreply.github.com>
2022-07-03 02:15:21 -0300

    vuelvo a subir el parcial 3 con algunas correcciones más

--- parciales_viejos_de_practica/src/Parcial_de_practica3.java
+++ parciales_viejos_de_practica/src/Parcial_de_practica3.java
@@ -10,20 +10,27 @@ secuencia.*/
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Random; //(esto no iría es sólo para probar de cargar la matriz a ver si anda)
 //importar librerias necesarias para los métodos preexistentes;
 public class Parcial_de_practica3 {
 	public static final int MAXFILA = 10;
 	public static final int MAXCOLUMNA = 30;
+	public static final int MAXVALOR = 9;//(estas constantes no irían son solo para probar el código)
+	public static final int MINVALOR = 1;//(estas constantes no irían son solo para probar el código)
+	public static final double PROBABILIDAD_NUMERO = 0.4; //(estas constantes no irían son solo para probar el código)
 	public static void main(String[] args) {
 		int numeroIngresado;
 		int [][]matriz = new int [MAXFILA][MAXCOLUMNA];
 		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-		//cargar_la_matriz_secuencias_int(matriz);
+		cargar_la_matriz_secuencias_int(matriz); //(la cargo para probar el código pero ésta línea no iría al igual que el método de carga implementado abajo)
+		imprimir_la_matriz(matriz);
 		try{
 			System.out.println("Por favor ingrese un numero: ");
 			numeroIngresado = Integer.valueOf(entrada.readLine());
 			for (int fila=0; fila < MAXFILA; fila++){
 				reemplazar_en_cada_secuencia(matriz, fila, numeroIngresado);
+				System.out.println("En la fila "+fila+" la secuencia queda así: ");
+				imprimir_la_matriz(matriz);
 			}
 		}
 		catch(Exception exc){
@@ -32,7 +39,7 @@ public class Parcial_de_practica3 {
 	}
 	
 	public static void reemplazar_en_cada_secuencia(int[][]matriz, int fila, int numeroIngresado){
-		int columna = 0, sumaDelContenido = 0, sizeSecuencia = 0, elMayorPar = 0, promedio = 0;
+		int columna = 0, sumaDelContenido = 0, sizeSecuencia = 0, elMayorPar = 0, promedio = 0, posicionMayorPar = 0;
 		while(columna<MAXCOLUMNA){
 			//encontrar una secuencia:
 			//si estoy en un número y en la anterior tengo un cero
@@ -46,6 +53,7 @@ public class Parcial_de_practica3 {
 					//ir buscando el mayor par hasta el proximo cero
 					if (((matriz[fila][columna]%2)==0) && (matriz[fila][columna]>elMayorPar)){
 						elMayorPar = matriz[fila][columna];
+						posicionMayorPar = columna;
 					}
 					
 					//si columna+1 = cero
@@ -55,6 +63,7 @@ public class Parcial_de_practica3 {
 						//si suma de todo el contenido es < numeroIngresado && promedio < mayor numero (que entiendo que también podria ser el mayor par porque no hay impares)
 						if ((sumaDelContenido < numeroIngresado) && (promedio < elMayorPar)){
 							//reemplazar en la pos del mayor par con la suma de todo el contenido
+							matriz[fila][posicionMayorPar] = sumaDelContenido;
 						}
 						//suma del contenido = 0;
 						sumaDelContenido = 0;
@@ -62,6 +71,7 @@ public class Parcial_de_practica3 {
 						sizeSecuencia = 0;
 						//mayor par = 0;
 						elMayorPar = 0;
+						posicionMayorPar =0;
 					}
 					columna++;
 				}
@@ -75,4 +85,38 @@ public class Parcial_de_practica3 {
 /*_dado un numero ingresado por el usuario, reemplace de cada secuencia el mayor valor par que posee por lo que suma el contenido de la secuencia, 
 si dicha suma es menor al numero ingresado y el promedio entero del contenido de la secuencia es menor al mayor valor que posee la
 secuencia.*/
+	
+	public static void cargar_la_matriz_secuencias_int(int[][]matriz){
+		for (int fila = 0; fila < MAXFILA; fila++){
+			cargar_arreglo_aleatorio_secuencias_int(matriz[fila]);
+		}
+		System.out.println("");
+	}
+
+	public static void cargar_arreglo_aleatorio_secuencias_int(int [] arr){
+		Random r = new Random();
+		arr[0] = 0;
+		arr[MAXCOLUMNA-1] = 0;
+		for (int pos = 1; pos < MAXCOLUMNA-1; pos++){
+			if (r.nextDouble()>PROBABILIDAD_NUMERO){
+				int aux = (r.nextInt(MAXVALOR+MINVALOR-1) + MINVALOR);
+				while (aux%2!=0)
+				 aux=(r.nextInt(MAXVALOR+MINVALOR-1) + MINVALOR);
+				arr[pos] = aux;
+			}
+			else{
+				arr[pos]=0;				
+			}
+		}
+	}
+	
+
+	public static void imprimir_la_matriz(int[][]matriz){
+		for (int fila =0; fila<MAXFILA; fila++){
+			for(int columna = 0; columna<MAXCOLUMNA; columna++){
+				System.out.print("[" + matriz[fila][columna] + "]");
+			}
+			System.out.println("");
+		}
+	}
 }
