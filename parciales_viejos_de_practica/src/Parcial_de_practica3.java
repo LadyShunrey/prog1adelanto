83c6659116245b963725c5cb543f72ac475d4eb7
Melisa Burlando <90287257+LadyShunrey@users.noreply.github.com>
2022-07-03 01:46:13 -0300

    subo corregido y completo el parcial 3

--- parciales_viejos_de_practica/src/Parcial_de_practica3.java
+++ parciales_viejos_de_practica/src/Parcial_de_practica3.java
@@ -32,25 +32,47 @@ public class Parcial_de_practica3 {
 	}
 	
 	public static void reemplazar_en_cada_secuencia(int[][]matriz, int fila, int numeroIngresado){
-		int columna = 0;
+		int columna = 0, sumaDelContenido = 0, sizeSecuencia = 0, elMayorPar = 0, promedio = 0;
 		while(columna<MAXCOLUMNA){
 			//encontrar una secuencia:
-			//si estoy en un número y en columna-1 tengo un cero
+			//si estoy en un número y en la anterior tengo un cero
+			if ((matriz[fila][columna]!=0) && (matriz[fila][columna-1]==0)){
 				//MIENTRAS que columna != 0
+				while (matriz[fila][columna]!=0){	
 					//ir sumando todo el contenido hasta el proximo cero
+					sumaDelContenido += matriz[fila][columna];
 					//ir sumando el tamaño de la secuencia hasta el proximo cero
+					sizeSecuencia++;
 					//ir buscando el mayor par hasta el proximo cero
-					//ir buscando el mayor número sea par o impar (aunque el enunciado dice que son todos pares)
+					if (((matriz[fila][columna]%2)==0) && (matriz[fila][columna]>elMayorPar)){
+						elMayorPar = matriz[fila][columna];
+					}
+					
 					//si columna+1 = cero
+					if (matriz[fila][columna+1]==0){
 						//promedio del contenido = suma de todo el contenido / tamaño de la secuencia
-						//si suma de todo el contenido es < numeroIngresado && promedio > mayor numero (que entiendo que también podria ser el mayor par porque no hay impares)
+						promedio = sumaDelContenido / sizeSecuencia;
+						//si suma de todo el contenido es < numeroIngresado && promedio < mayor numero (que entiendo que también podria ser el mayor par porque no hay impares)
+						if ((sumaDelContenido < numeroIngresado) && (promedio < elMayorPar)){
 							//reemplazar en la pos del mayor par con la suma de todo el contenido
+						}
 						//suma del contenido = 0;
+						sumaDelContenido = 0;
 						//suma de tamaño = 0;
+						sizeSecuencia = 0;
 						//mayor par = 0;
-						//mayor numero = 0;
-			columna++;	
+						elMayorPar = 0;
+					}
+					columna++;
+				}
+			}
+			
+			else {
+				columna++;
+			}
 		} 
 	}
-
+/*_dado un numero ingresado por el usuario, reemplace de cada secuencia el mayor valor par que posee por lo que suma el contenido de la secuencia, 
+si dicha suma es menor al numero ingresado y el promedio entero del contenido de la secuencia es menor al mayor valor que posee la
+secuencia.*/
 }
